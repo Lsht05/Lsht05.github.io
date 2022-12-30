4013d7e7bc6d1e5157caba3d00d16c41a3e4d4e7
Luis Salvador Heysen Taborda <lsalvador.ht@gmail.com>
2022-12-29 20:49:59 -0500

    new file:   img/EntregarBandera.jpeg 	modified:   img/main.png 	modified:   index.html 	modified:   js/script.js 	modified:   math_book.html 	new file:   pdfs/PartialVersion.pdf

--- js/script.js
+++ js/script.js
@@ -19,12 +19,12 @@ window.onscroll = () => {
 
 const sr = ScrollReveal ({
 	distance: '45px',
-	duration: 2700,
+	duration: 10,
 	reset: true
 })
 
-sr.reveal('.home-text',{delay:350, origin:'left'})
-sr.reveal('.home-img',{delay:350, origin:'right'})
+sr.reveal('.home-text',{delay:10, origin:'left'})
+sr.reveal('.home-img',{delay:10, origin:'right'})
 
-sr.reveal('.sub-service,.about,.portfolio,.service,.cta,.contact',{delay:200, origin:'bottom'})
+sr.reveal('.sub-service,.about,.portfolio,.service,.cta,.contact',{delay:10, origin:'bottom'})
 
